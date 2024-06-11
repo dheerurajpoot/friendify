85a2cf5b3983317f086a3f9bf3bfdc6bf79c584a
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-06-11 16:22:40 +0530

    setup ShadCN UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,18 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Friendify: Connect with your friends",
-  description: "Connect with your friends everywere in this World",
+	title: "Friendify: Connect with your friends",
+	description: "Connect with your friends everywere in this World",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }

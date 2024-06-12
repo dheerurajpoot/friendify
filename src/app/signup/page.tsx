67a96237677e427999cc97795ecba37b37168760
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-06-12 19:07:35 +0530

    signin and signup update

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,8 +6,8 @@ import React from "react";
 
 const Signup = () => {
 	return (
-		<div className='flex flex-col min-h-sm relative items-center justify-center pt-5 m-auto lg:w-[900px] md:w-[900px]'>
-			<div className='flex min-h-screen items-center justify-center dark:bg-gray-950'>
+		<div className='flex flex-col mt-[10%] relative items-center justify-center pt-5 m-auto lg:w-[900px] md:w-[900px]'>
+			<div className='flex items-center justify-center dark:bg-gray-950'>
 				<div className='w-full max-w-md space-y-6 rounded-lg bg-white p-8 shadow-lg dark:bg-gray-800'>
 					<div className='space-y-2 text-center'>
 						<h1 className='text-3xl font-bold'>Sign Up</h1>

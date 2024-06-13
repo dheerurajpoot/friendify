559e082439a1c4d5309e7905e06364a25f01833c
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-06-13 17:13:38 +0530

    add dbconnect

---
+++ src/lib/db.ts
@@ -0,0 +1,19 @@
+import mongoose, { Connection } from 'mongoose'
+
+let isConnected: Connection | boolean = false;
+
+export const connectDb = async ()=>{
+  if (isConnected) {
+    console.log("Database already connected!");
+    return isConnected;
+  }
+  try {
+    const res = await mongoose.connect(process.env.MONGO_URI!)
+    isConnected = res.connection;
+    console.log("Database connected Successfully");
+    return isConnected
+  } catch (error) {
+    console.log(error);
+    throw error
+  }
+}

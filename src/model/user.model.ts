7f1720699ade1d9f9abcbccdd842fe7f9b9883fe
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-06-17 21:18:07 +0530

    update usermodel

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,37 +1,54 @@
-import mongoose, { Document, Model } from 'mongoose'
+import mongoose, { Document, Model } from "mongoose";
 
-export interface userInterface{
-  name: string,
-  username: string,
-  email: string,
-  profilepic: string,
-  password: string
+export interface userInterface {
+	name: string;
+	username: string;
+	email: string;
+	profilepic: string;
+	password: string;
+	isVerified: Boolean;
+	forgotPasswordToken: string;
+	forgotPasswordTokenExpiry: string;
+	verifyToken: string;
+	verifyTokenExpiry: string;
 }
-export interface userDocument extends userInterface, Document{
-  createdAt: Date,
-  updatedAt: Date
+export interface userDocument extends userInterface, Document {
+	createdAt: Date;
+	updatedAt: Date;
 }
 
-const userModel = new mongoose.Schema<userDocument>({
-    name:{
-        type:String,
-    },
-    username:{
-        type:String,
-        unique: true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    profilepic:{
-        type:String,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-}, {timestamps:true});
+const userModel = new mongoose.Schema<userDocument>(
+	{
+		name: {
+			type: String,
+		},
+		username: {
+			type: String,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		profilepic: {
+			type: String,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		isVerified: {
+			type: Boolean,
+			default: false,
+		},
+		forgotPasswordToken: String,
+		forgotPasswordTokenExpiry: Date,
+		verifyToken: String,
+		verifyTokenExpiry: Date,
+	},
+	{ timestamps: true }
+);
 
-export const User : Model<userDocument> = mongoose?.models?.User || mongoose.model('User', userModel);
+export const User: Model<userDocument> =
+	mongoose?.models?.User || mongoose.model("User", userModel);

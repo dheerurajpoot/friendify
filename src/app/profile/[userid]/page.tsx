1417dc5216d08711b8c8ac45ca11e97aa72957c0
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-07-10 09:22:21 +0530

    bug fixes in profile page

--- src/app/profile/[userid]/page.tsx
+++ src/app/profile/[userid]/page.tsx
@@ -49,7 +49,7 @@ const Profile = ({ params }: { params: { userid: string } }) => {
 
 	useEffect(() => {
 		getProfile();
-	}, [userId, getProfile]);
+	}, [userId]);
 
 	useEffect(() => {
 		if (user?.followers.length === 0) return;

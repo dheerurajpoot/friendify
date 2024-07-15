02d18e341e9214a462e729339c74e511da04ceb7
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-07-15 22:14:15 +0530

    Update route.ts

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,21 +1,47 @@
 import { connectDb } from "@/dbConfig/connectDb";
+import { log } from "console";
 import { NextResponse } from "next/server";
 
 connectDb();
 
+// export async function GET() {
+// 	try {
+// 		const response = NextResponse.json({
+// 			message: "Logout Successfully",
+// 			success: true,
+// 		});
+// 		response.cookies.set("token", "", {
+// 			httpOnly: true,
+// 			expires: new Date(0),
+// 			path: "/",
+// 		});
+// 		return response;
+// 	} catch (error: any) {
+// 		return NextResponse.json({ error: error.message }, { status: 500 });
+// 	}
+// }
+
 export async function GET() {
 	try {
-		const response = NextResponse.json({
-			message: "Logout Successfully",
-			success: true,
-		});
+		const response = new NextResponse(); // Create a new response object
+
+		// Set the JSON response body
+		// response.json({
+		// 		message: "Logout Successfully",
+		// 		success: true,
+		// });
+
+		// Set the cookie to expire immediately (by setting expiry to a date in the past)
 		response.cookies.set("token", "", {
 			httpOnly: true,
 			expires: new Date(0),
 			path: "/",
 		});
+
 		return response;
 	} catch (error: any) {
-		return NextResponse.json({ error: error.message }, { status: 500 });
+		// Return an error response with status 500 if there's an error
+		// return new NextResponse.json({ error: error.message }, { status: 500 });
+		console.log(error);
 	}
 }

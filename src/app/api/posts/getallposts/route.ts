f32207c977f6c22793a2f5552045bb27c8890191
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2024-07-17 18:11:04 +0530

    update getallpost route

--- src/app/api/posts/getallposts/route.ts
+++ src/app/api/posts/getallposts/route.ts
@@ -32,47 +32,20 @@ export async function GET(request: NextRequest) {
 	try {
 		await connectDb();
 
-		const posts: postDocument[] = await Post.find().lean();
-
-		const populatedPosts: PopulatedPost[] = await Promise.all(
-			posts.map(async (post) => {
-				const createdBy = post.createdBy
-					? await User.findById(post.createdBy)
-							.select("name profilepic")
-							.lean()
-					: null;
-				const comments = await Promise.all(
-					post.comments.map(async (comment) => {
-						const author = comment.author
-							? await User.findById(comment.author)
-									.select("name profilepic")
-									.lean()
-							: null;
-						return {
-							...comment,
-							author: author
-								? {
-										_id: author._id as Types.ObjectId,
-										name: author.name,
-										profilepic: author.profilepic,
-								  }
-								: null,
-						} as unknown as PopulatedComment;
-					})
-				);
-				return {
-					...post,
-					createdBy: createdBy
-						? {
-								_id: createdBy._id as Types.ObjectId,
-								name: createdBy.name,
-								profilepic: createdBy.profilepic,
-						  }
-						: null,
-					comments,
-				} as unknown as PopulatedPost;
+		// Populate post documents with createdBy and comments.author
+		const populatedPosts = await Post.find()
+			.populate({
+				path: "createdBy",
+				select: "name profilepic",
 			})
-		);
+			.populate({
+				path: "comments",
+				populate: {
+					path: "author",
+					select: "name profilepic",
+				},
+			})
+			.lean();
 
 		return NextResponse.json({
 			message: "Posts Found",

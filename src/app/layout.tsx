d8103b56eb2cc5c1f4ded3e1ec16d053f8e7bb2f
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2025-03-05 10:52:25 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,34 @@
-// app/layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import LayoutWrapper from "@/components/LayoutWrapper";
+import Script from "next/script"; 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "HikeTok: Connect with your friends",
-	description: "Connect with your friends everywhere in the World",
+  title: "HikeTok: Connect with your friends",
+  description: "Connect with your friends everywhere in the World",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang='en'>
-			 <head>
-      				 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8559499402519284"
-     crossorigin="anonymous"></script>
-     			 </head>
-			<body className={inter.className}>
-				<LayoutWrapper>{children}</LayoutWrapper>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        <Script
+          async
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8559499402519284"
+          crossOrigin="anonymous" 
+        />
+      </head>
+      <body className={inter.className}>
+        <LayoutWrapper>{children}</LayoutWrapper>
+      </body>
+    </html>
+  );
 }
